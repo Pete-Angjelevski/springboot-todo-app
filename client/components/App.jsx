0ee93563d903f7b67b14c015d4006eb2fa372ba5
Pete Angjelevski <p.angjelevski@gmail.com>
2021-04-22 15:18:50 +1200

    render toDoItems from backend

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+  const [todoItems, setTodoItems] = useState(null)
+
   useEffect(() => {
     fetch('http://localhost:8080/api/v1/todoItems')
       .then(res => res.json())
-      .then(todoItems => console.log(todoItems))
+      .then(data => {
+        console.log('Todo Items list: ', data)
+        setTodoItems(data)
+        return null
+      })
       .catch(err => console.log(err))
   }, [])
 
   return (
-    <h1>React development has begun!</h1>
+    <div>{todoItems ? todoItems.map(todoItem => {
+      return (
+        <div key={todoItem.id}>
+          <p>{todoItem.task}</p>
+          <input type="checkbox" checked={todoItem.isDone} onChange={}></input>
+        </div>
+      )
+    }) : 'loading data ...'}
+    </div>
+
   )
 }
 
