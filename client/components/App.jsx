c1ea46f5de21a9712cc157dd91247176f6c4af95
Pete Angjelevski <p.angjelevski@gmail.com>
2021-04-22 14:10:13 +1200

    fetch backend springboot info

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const App = () => {
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/todoItems')
+      .then(res => res.json())
+      .then(todoItems => console.log(todoItems))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <h1>React development has begun!</h1>
   )
--- todo-app/src/main/java/com/peteangjelevski/todoapp/web/TodoController.java
+++ todo-app/src/main/java/com/peteangjelevski/todoapp/web/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.peteangjelevski.todoapp.domain.TodoItem;
 import com.peteangjelevski.todoapp.service.TodoService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 	
 	@Autowired
